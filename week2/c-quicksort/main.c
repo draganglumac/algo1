10b599ddf9f889697c1307522aa5f7c51867f282
draganglumac <dragan.glumac@gmail.com>
2014-10-23 19:15:53 +0100

    Added CppUnitLite.

---
+++ week2/c-quicksort/main.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  23/10/2014 14:27:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dragan Glumac (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "quicksort.h"
+
+int main() {
+	printf("All is well!\n");
+	return 0;
+}
---
+++ week2/c-quicksort/quicksort.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  quicksort.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  23/10/2014 14:28:35
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dragan Glumac (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef __QUICKSORT_H__
+#define __QUICKSORT_H__
+
+void quicksort(int *a, int size);
+
+#endif // __QUICKSORT_H__
