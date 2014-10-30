a28e956527a36299a0015caca54b69bd22f18d7e
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-10-30 09:51:11 +0000

    Small test file added.

--- week3/karger/driver.rb
+++ week3/karger/driver.rb
@@ -9,7 +9,8 @@ require 'min_cut'
 class Driver
   def run
     min_cut = MinCut.new
-    graph = Graph.load('kargerMinCut.txt')
+    # graph = Graph.load('kargerMinCut.txt')
+    graph = Graph.load('2cut.txt')
     cut = min_cut.compute_cut(graph)
     pretty_print(graph, cut)
   end
