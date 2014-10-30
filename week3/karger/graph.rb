0e2ce213ce606dc65045777765b205a5d6a40b60
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-10-30 11:23:18 +0000

    Contracting edges now, test in place for it too.

--- week3/karger/graph.rb
+++ week3/karger/graph.rb
@@ -2,7 +2,7 @@ class Graph
   attr_accessor :num_nodes
 
   def initialize
-    @graph = []
+    @graph = [nil]
     @num_nodes = 0
   end
 
@@ -22,9 +22,39 @@ class Graph
   def to_s
     s = ''
     @graph.each do |el|
-      el.each { |e| s += '%3d ' % e }
-      s[-1] = "\n"
+      unless el.nil?
+        el.each { |e| s += '%3d ' % e }
+        s[-1] = "\n"
+      end
     end
     s
+  end
+
+  def contract_edge(v1, v2)
+    v1_list = @graph[v1]
+    @graph[v1] = nil
+    replace_in_graph(v1, v2)
+    v2_list = merge_lists_omitting(v1_list, @graph[v2], v2)
+    @graph[v2] = v2_list.sort!
+  end
+
+  def replace_in_graph(v1, v2)
+    @graph.each do |vl|
+      unless vl.nil?
+        i = vl.find_index(v1)
+        until i.nil?
+          vl[i] = v2
+          i = vl.find_index(v1)
+        end
+        vl.sort!
+      end
+    end
+  end
+
+  def merge_lists_omitting(l1, l2, el_to_omit)
+    result = l1 + l2
+    result.sort!
+    result.delete(el_to_omit)
+    result
   end
 end
--- week3/karger/spec/graph_spec.rb
+++ week3/karger/spec/graph_spec.rb
@@ -1,4 +1,5 @@
 $: << '.'
+$: << '..'
 
 require 'rspec'
 require 'graph'
@@ -6,9 +7,11 @@ require 'graph'
 describe 'Graph behaviour' do
 
   let(:graph) { Graph.new }
-
+  before(:each) { graph.load('2cut.txt') }
+  # --1
   it 'should load a graph from a file into a list of adjacency lists' do
     expected = [
+        nil,
         [2, 3, 4, 7],
         [1, 3, 4],
         [1, 2, 4],
@@ -18,7 +21,22 @@ describe 'Graph behaviour' do
         [1, 5, 6, 8],
         [5, 6, 7]
     ]
-    graph.load '2cut.txt'
+    expect(graph.adj_list).to eq(expected)
+  end
+  # --2
+  it 'should contract an edge 1, 4 correctly' do
+    expected = [
+        nil,
+        nil,
+        [3, 4, 4],
+        [2, 4, 4],
+        [2, 2, 3, 3, 5, 7],
+        [4, 6, 7, 8],
+        [5, 7, 8],
+        [4, 5, 6, 8],
+        [5, 6, 7]
+    ]
+    graph.contract_edge(1, 4)
     expect(graph.adj_list).to eq(expected)
   end
 end
