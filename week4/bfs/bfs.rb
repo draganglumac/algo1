4a95f018347fec8078c2d94a823124789431ac9f
draganglumac <dragan.glumac@gmail.com>
2014-11-02 09:54:20 +0000

    Shortest path added to BFS class.

--- week4/bfs/bfs.rb
+++ week4/bfs/bfs.rb
@@ -15,4 +15,49 @@ class BFS
     reachable.sort
   end
 
+  def shortest_path(graph, source, dest)
+    queue = Queue.new
+    visited = []
+    distance_to_dest = 0
+
+    queue.push [source, 0]
+    until queue.empty?
+      current, level = queue.pop
+      visited << [current, level] unless explored?(visited, current)
+
+      if current == dest
+        distance_to_dest = level
+        break
+      end
+
+      graph.edges_for_node(current).each do |e|
+        queue.push [e, level + 1] unless explored?(visited, e)
+      end
+    end
+
+    path = [dest]
+    current = dest
+    previous_level = level_for_node(visited, current) - 1
+    until current == source
+      graph.edges_for_node(current).each do |e|
+        if explored?(visited, e) and level_for_node(visited, e) == previous_level
+          current = e
+          previous_level -= 1
+          path << current
+          break
+        end
+      end
+    end
+
+    [distance_to_dest, path.reverse]
+  end
+
+  def explored?(nodes_and_levels, node)
+    (nodes_and_levels.map { |e| e.first }).include? node
+  end
+
+  def level_for_node(nodes_and_levels, node)
+    i = (nodes_and_levels.map { |e| e.first }).index node
+    nodes_and_levels[i].last
+  end
 end
--- week4/bfs/driver.rb
+++ week4/bfs/driver.rb
@@ -11,22 +11,25 @@ class Driver
     bfs = BFS.new
     graph = Graph.new
 
-    # graph.load('kargerMinCut.txt')
+    graph.load('kargerMinCut.txt')
     # graph.load('2cut.txt')
     # graph.load('2permuted.txt')
     # graph.load('1cut.txt')
     # graph.load('1permuted.txt')
-    graph.load('3cut.txt')
+    # graph.load('3cut.txt')
     # graph.load('3cut-small.txt')
 
     reachable_from_1 = bfs.nodes_reachable_from(graph, 1)
-    pretty_print(reachable_from_1)
+    dist, path = bfs.shortest_path(graph, 1, 38)
+    pretty_print(reachable_from_1, [dist, path])
   end
 
-  def pretty_print(nodes)
+  def pretty_print(nodes, shortest_path)
     p nodes
     puts ''
     puts "Number of reachable nodes = #{nodes.size}"
+    puts ''
+    p shortest_path
   end
 
 end
