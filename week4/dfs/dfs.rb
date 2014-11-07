2c9818150717de48f396318602bc4acc8d777091
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-11-07 10:18:47 +0000

    Implemeted DFS using a stack as an alternative.

--- week4/dfs/dfs.rb
+++ week4/dfs/dfs.rb
@@ -1,14 +1,24 @@
-require 'thread'
+require 'stack'
 
 class DFS
   def nodes_reachable_from(graph, start_node)
-    # dfs_stack graph, start_node
-    dfs graph, start_node
+    dfs_stack graph, start_node
+    # dfs graph, start_node
   end
 
   private
   def dfs_stack(graph, start_node)
-    # ToDo
+    visited = {}
+    stack = Stack.new
+    stack.push start_node
+    until stack.empty?
+      node = stack.pop
+      if visited[node].nil?
+        visited[node] = true
+        graph.edges_for_node(node).each { |e| stack.push e }
+      end
+    end
+    visited.keys.sort
   end
 
   def dfs(graph, start_node)
---
+++ week4/dfs/stack.rb
@@ -0,0 +1,17 @@
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def push(e)
+    @stack << e
+  end
+
+  def pop
+    @stack.delete_at(@stack.size - 1)
+  end
+
+  def empty?
+    @stack.empty?
+  end
+end
