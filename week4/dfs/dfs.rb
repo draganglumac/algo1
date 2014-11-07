17983fefb25efc179c5e1d9b3b5b8480b7a8d224
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-11-07 09:34:13 +0000

    Adding dfs project.

---
+++ week4/dfs/dfs.rb
@@ -0,0 +1,17 @@
+require 'thread'
+
+class DFS
+  def nodes_reachable_from(graph, start_node)
+    dfs_stack graph, start_node
+    # dfs_recursive graph, start_node
+  end
+
+  private
+  def dfs_stack(graph, start_node)
+    # ToDo
+  end
+
+  def dfs_recursive(graph, start_node)
+    # ToDo
+  end
+end
---
+++ week4/dfs/driver.rb
@@ -0,0 +1,34 @@
+require 'pry'
+require 'pry-nav'
+
+$: << '.'
+
+require 'graph'
+require 'dfs'
+
+class Driver
+  def run
+    dfs = DFS.new
+    graph = Graph.new
+
+    graph.load('kargerMinCut.txt')
+    # graph.load('2cut.txt')
+    # graph.load('2permuted.txt')
+    # graph.load('1cut.txt')
+    # graph.load('1permuted.txt')
+    # graph.load('3cut.txt')
+    # graph.load('3cut-small.txt')
+
+    reachable_from_1 = dfs.nodes_reachable_from(graph, 1)
+    pretty_print(reachable_from_1)
+  end
+
+  def pretty_print(nodes)
+    p nodes
+    puts ''
+    puts "Number of reachable nodes = #{nodes.size}"
+  end
+
+end
+
+Driver.new.run
