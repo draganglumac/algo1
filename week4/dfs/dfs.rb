873cc39b8b43b32aae8f729b5095240ccae2a489
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-11-07 10:04:21 +0000

    Recursive DFS implemented.

--- week4/dfs/dfs.rb
+++ week4/dfs/dfs.rb
@@ -2,8 +2,8 @@ require 'thread'
 
 class DFS
   def nodes_reachable_from(graph, start_node)
-    dfs_stack graph, start_node
-    # dfs_recursive graph, start_node
+    # dfs_stack graph, start_node
+    dfs graph, start_node
   end
 
   private
@@ -11,7 +11,19 @@ class DFS
     # ToDo
   end
 
-  def dfs_recursive(graph, start_node)
-    # ToDo
+  def dfs(graph, start_node)
+    visited = dfs_recursive(graph, start_node, {})
+    visited.keys.sort
+  end
+
+  def dfs_recursive(graph, start_node, visited)
+    if visited[start_node].nil?
+      visited[start_node] = true
+      edges = graph.edges_for_node start_node
+      edges.each do |e|
+        visited = dfs_recursive graph, e, visited
+      end
+    end
+    visited
   end
 end
