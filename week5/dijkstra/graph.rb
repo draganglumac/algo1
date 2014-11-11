df6699f1b689b7764d077aca308cb4afd2783323
draganglumac <dragan.glumac@gmail.com>
2014-11-11 14:29:13 +0000

    Using the trusty old graph implementation, just adding weights to edges.

---
+++ week5/dijkstra/graph.rb
@@ -0,0 +1,122 @@
+class InvalidEdgeException < Exception
+end
+
+class Graph
+  attr_accessor :adj_list
+
+  def initialize
+    @adj_list = [nil]
+  end
+
+  def clone
+    g = Graph.new
+    new_list = []
+    @adj_list.each do |el|
+      if el.nil?
+        new_list << nil
+      else
+        new_list << el.clone
+      end
+    end
+    g.adj_list = new_list
+    g
+  end
+
+  def prepare_for_reversal
+    @adj_list.each do |list|
+      unless list.nil?
+        list.delete(list.first) if list.first.nil?
+        list << nil unless list.last.nil?
+      end
+    end
+  end
+
+  def clean_up_after_reversal
+    @adj_list.each do |list|
+      unless list.nil?
+        list.delete(list.first) if list.first.nil?
+        list.delete(list.last)  if list.last.nil?
+      end
+    end
+  end
+
+  def nodes
+    nodes = []
+    (1...@adj_list.size).each { |i| nodes << i unless @adj_list[i].nil? }
+    nodes
+  end
+
+  def edges_for_node(n)
+    @adj_list[n] || []
+  end
+
+  def load(filename, sorted=false)
+    @adj_list = [nil]
+    File.open(filename, 'r').each do |line|
+      unless line.empty?
+        list = line.strip.split(' ').map { |el| el.to_i }
+        add_to_node(list[0], list[1..-1], sorted)
+      end
+    end
+  end
+
+  def add_to_node(node, edges, sorted)
+      @adj_list[node] ||= []
+      @adj_list[node] += edges
+      @adj_list[node].sort! if sorted
+  end
+
+  def to_s
+    s = ''
+    i = 0
+    @adj_list.each do |el|
+      unless el.nil?
+        s += '%3d -> ' % i
+        el.each { |e| s += '%3d ' % e }
+        s[-1] = "\n"
+      end
+      i += 1
+    end
+    s
+  end
+
+  def valid_node(v)
+    v > 0 and v < @adj_list.size and (not @adj_list[v].nil?)
+  end
+
+  def valid_edge(v1, v2)
+    valid_node v1 and valid_node v2 and @adj_list[v1].member?(v2)
+  end
+
+  def contract_edge(v1, v2)
+    unless valid_edge v1, v2
+      raise InvalidEdgeException.new "Invalid edge (#{v1}, #{v2})"
+    end
+
+    v1_list = @adj_list[v1]
+    @adj_list[v1] = nil
+    replace_in_graph(v1, v2)
+    v2_list = merge_lists_omitting(v1_list, @adj_list[v2], v2)
+    @adj_list[v2] = v2_list.sort!
+  end
+
+  def replace_in_graph(v1, v2)
+    @adj_list.each do |vl|
+      unless vl.nil?
+        i = vl.find_index(v1)
+        until i.nil?
+          vl[i] = v2
+          i = vl.find_index(v1)
+        end
+        vl.sort!
+      end
+    end
+  end
+
+  def merge_lists_omitting(l1, l2, el_to_omit)
+    result = l1 + l2
+    result.sort!
+    result.delete(el_to_omit)
+    result
+  end
+end
--- week5/dijkstra/graph/node.rb
+++
@@ -1,8 +0,0 @@
-
-class Node
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
