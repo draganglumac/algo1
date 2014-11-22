3c4eebcf184e5549c2243618a8180cdc812b8921
draganglumac <dragan.glumac@gmail.com>
2014-11-22 11:22:29 +0000

    Hash table solution - slooooow.

--- week6/twosum/spec/two_sum_spec.rb
+++ week6/twosum/spec/two_sum_spec.rb
@@ -9,14 +9,38 @@ describe TwoSum do
   let(:two_sum) { TwoSum.new }
 
   it 'should return empty result for empty array' do
-    expect(two_sum.pairs_for_sum [], 2709).to eq([])
+    two_sum.set_array []
+    expect(two_sum.pairs_for_sum 2709).to eq([])
   end
 
   it 'should return empty result for array with one element' do
-    expect(two_sum.pairs_for_sum([1], 2709)).to eq([])
+    two_sum.set_array [1]
+    expect(two_sum.pairs_for_sum 2709).to eq([])
   end
 
   it 'should return a pair of elements that sums up to the sum' do
-    expect(two_sum.pairs_for_sum [1, 2000, 2, 709], 2709).to eq([[2000, 709], [709, 2000]])
+    two_sum.set_array [1, 1, 2000, 2, 709]
+    expect(two_sum.pairs_for_sum 2709).to eq([[2000, 709], [709, 2000]])
   end
+
+  it 'should return distinct sums for an interval' do
+    two_sum.set_array [1, 2000, 2, 709]
+    expect(two_sum.distinct_sums 0..2800).to eq([3, 710, 711, 2001, 2002, 2709])
+  end
+
+  describe 'tests from discussion forums' do
+    def run_test(two_sum, file, output)
+      two_sum.load_array_from_file file
+      expect(two_sum.distinct_sums -10000..10000).to eq(output)
+    end
+
+    it 'should pass test 1' do
+      run_test two_sum, 'test/test1.txt', [-10000, -9999, 1, 2, 3]
+    end
+
+    it 'should pass test 2' do
+      run_test two_sum, 'test/test2.txt', [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14]
+    end
+  end
+
 end
--- week6/twosum/two_sum.rb
+++ week6/twosum/two_sum.rb
@@ -1,17 +1,77 @@
 class TwoSum
 
-  def pairs_for_sum(array, sum)
-    indicators = {}
-    array.each { |e| indicators[e] = true }
+  def set_array(array)
+    @array = array
+    @indicators = {}
+    @array.each { |e| @indicators[e] = true }
+  end
 
-    pairs = []
-    array.each do |e|
-      if indicators[sum - e]
-        pairs << [e, sum - e]
+  def load_array_from_file(file)
+    array = []
+    File.open(file, 'r').each do |line|
+      n = line.strip.to_i
+      if n == 0
+        if line.strip == '0'
+          array << n
+        end
+      else
+        array << n
       end
     end
+    set_array array
+  end
 
+  def pairs_for_sum(sum)
+    pairs = []
+    unless @array.nil? or @array.empty?
+      @array.each do |e|
+        if @indicators[sum - e]
+          pairs << [e, sum - e] unless sum == 2 * e
+        end
+      end
+    end
     pairs
+  end
+
+  def distinct_sums(interval)
+    result = {}
+    # fast_distinct_sums(interval, result)
+    hash_distinct_sums(interval, result)
+    # two_loops_distinct_sums(interval, result)
+    result.keys.sort
+  end
+
+  def fast_distinct_sums(interval, result)
+    array = @array.sort
+    max_j = array.size - 1
+    min_i = 0
+
+  end
+
+  def hash_distinct_sums(interval, result)
+    interval.each do |sum|
+      @array.each do |n|
+        unless sum == 2 * n
+          if @indicators[sum - n]
+            result[sum] = true
+            break
+          end
+        end
+      end
+    end
+  end
+
+  def two_loops_distinct_sums(interval, result)
+    i = 0
+    j = 1
+    (i...@array.size).each do |first|
+      (j...@array.size).each do |second|
+        unless @array[first] == @array[second]
+          sum = @array[first] + @array[second]
+          result[sum] = true if interval.first <= sum and sum <= interval.last
+        end
+      end
+    end
   end
 
 end
